d75433c5e5613f42c42543d383655d21495d9236
Abhishek Bhargava <bhargavaabhishek2002@gmail.com>
2026-03-24 15:28:55 +0530

    fixed the bug

---
+++ test/app.test.js
@@ -0,0 +1,23 @@
+const { work } = require("../controller/home.controller");
+
+describe("Work Flow", () => {
+  test("server is working properly", () => {
+    // Arrange
+    const req = {};
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    // Act
+    work(req, res);
+
+    // Assert
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      success: true,
+      message: "Server Working",
+    });
+  });
+});
