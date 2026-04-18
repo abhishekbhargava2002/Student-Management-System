7685136f863fdbf76e87305071bd3e1d2107e5a9
Abhishek Bhargava <bhargavaabhishek2002@gmail.com>
2026-04-18 23:51:03 +0530

    fixed the issue

--- model/StudentModel/studentcourse.model.js
+++ model/StudentModel/studentcourse.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-//StudentCourse
+
 const studentSchema = new mongoose.Schema(
   {
     studentReferId: {
@@ -7,7 +7,7 @@ const studentSchema = new mongoose.Schema(
       ref: "studentRegistration",
       required: true,
     },
-    teacherReferId: { 
+    teacherReferId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "teacher",
     },
@@ -23,12 +23,29 @@ const studentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    department: { 
+    department: {
       type: String,
       required: true,
-    }
+    },
+    categoryNumber: {
+      type: Number,
+      unique: true,
+    },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
+
+// ✅ FIXED: Use correct model name 'studentcourse'
+studentSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const lastEntry = await mongoose
+      .model("studentcourse")
+      .findOne()
+      .sort({ categoryNumber: -1 }); // get highest categoryNumber
+
+    this.categoryNumber = lastEntry ? lastEntry.categoryNumber + 1 : 1;
+  }
+  next();
+});
 
 module.exports = mongoose.model("studentcourse", studentSchema);
